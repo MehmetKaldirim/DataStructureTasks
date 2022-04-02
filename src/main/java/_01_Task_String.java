35064fdd4361a1aac2f953539ab077c49c849d1f
Mehmet <mathdenizi@gmail.com>
2022-04-02 23:49:39 +0200

    first questions with hashmap also...

--- src/main/java/_01_Task_String.java
+++ src/main/java/_01_Task_String.java
@@ -1,24 +1,22 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class _01_Task_String {
     public static void main(String[] args) {
-        //String characters = "!veDJaCyd vaeo perelo xw";
-        //String document = "Cydeo Java Developer!";
+        String characters = "!veDJaCyd vaeo perelo xw";
+        String document = "Cydeo Java Developer!";
 
-        Scanner input = new Scanner(System.in);
+        /*Scanner input = new Scanner(System.in);
         System.out.println("Please enter first characters");
         String characters = input.next();
         System.out.println("Please enter document");
-        String document = input.next();
+        String document = input.next();*/
 
         generateWithString(characters,document);
         generateRecursive(characters,document);
         generateWithStream(characters,document);
-
+        System.out.println(generateDocumentWithHashMap(characters, document));
     }
 
     private static void generateWithStream(String characters, String document) {
@@ -48,4 +46,32 @@ public class _01_Task_String {
 
         System.out.println(result);
     }
+
+    public static boolean generateDocumentWithHashMap(String characters, String document) {
+
+        Map<Character, Integer> hashMap = new HashMap<>();
+        int count;
+
+        for (int i = 0; i < characters.length(); i++) {
+            if(hashMap.containsKey(characters.charAt(i))) {
+                count = hashMap.get(characters.charAt(i));
+                count++;
+                hashMap.replace(characters.charAt(i), count);
+
+            } else {
+                hashMap.put(characters.charAt(i), 1);
+            }
+        }
+
+        for (int i = 0; i < document.length(); i++) {
+            if(!hashMap.containsKey(document.charAt(i)) || hashMap.get(document.charAt(i))==0) {
+                return false;
+            } else {
+                count = hashMap.get(document.charAt(i));
+                count--;
+                hashMap.replace(document.charAt(i), count);
+            }
+        } return true;
+    }
+
 }
